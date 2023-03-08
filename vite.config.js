d2b9421addc2b1dfcec0cefffe7ec01ea1d3b92f
hasbai <jsclndnz@outlook.com>
2023-03-08 18:46:22 +0800

    feat: pwa

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
 import vuetify from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   resolve: {
@@ -18,6 +19,47 @@ export default defineConfig({
   base: '/',
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Content Management System',
+        short_name: 'CMS',
+        description: 'My personal content management system',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\/api\/.*?/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+            },
+          },
+          {
+            urlPattern: /(.*?)\.(js|css|ts)/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'js-css-cache',
+            },
+          },
+          {
+            urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+            },
+          },
+        ],
+      },
+    }),
     AutoImport({
       imports: [
         'vue',

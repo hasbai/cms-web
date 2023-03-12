03b911bcb7808f5106b7d7ebceb8b1f54de59b0c
hasbai <jsclndnz@outlook.com>
2023-03-13 00:39:29 +0800

    refactor: navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,9 @@
 <template>
-  <n-menu id="menu" :class="store.showMenu ? 'navi-show' : 'navi-hide'" :options="menuOptions"/>
+  <n-menu id="menu" :class="store.showMenu ? 'navi-show' : 'navi-hide'"
+          :options="menuOptions"
+          class=""
+          @click="onClick"
+  />
 </template>
 
 <script lang="ts" setup>
@@ -43,37 +47,60 @@ const menuOptions: MenuOption[] = [
     icon: renderIcon(IconHome)
   },
 ]
+
+function onClick() {
+  store.showMenu = false
+}
 </script>
 
 <style>
 #menu {
   flex-shrink: 0;
   transition: width 0.8s;
-}
-
-#menu + * {
-  transition: opacity 0.8s;
+  --navi-width: 0;
 }
 
 @media (min-width: 600px) {
-  .navi-show {
-    width: 300px;
+  #menu {
+    --navi-width: 300px;
   }
 }
 
 @media (max-width: 600px) {
-  .navi-show {
-    width: 50%;
+  #menu {
+    --navi-width: 50%;
   }
-}
-
-
-.navi-show + * {
-  opacity: 0.3
 }
 
 .navi-hide {
   width: 0;
 }
 
+.navi-show {
+  width: var(--navi-width);
+}
+
+.navi-show + * {
+  opacity: 0.3;
+}
+
+#menu + * {
+  transition: opacity 0.8s;
+}
+
+#menu {
+  pointer-events: none;
+}
+
+.navi-show::after {
+  content: '';
+  position: absolute;
+  display: block;
+  top: 0;
+  left: var(--navi-width);
+  width: calc(100vw - var(--navi-width));
+  height: 100%;
+  z-index: 100;
+  pointer-events: auto;
+}
 </style>
